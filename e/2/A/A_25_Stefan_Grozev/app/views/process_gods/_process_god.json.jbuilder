1b858c30602d1e1cb3521b2636a1833b389b81ab
StefotoG <stefan.grozev@yahoo.com>
2018-03-06 15:35:34 +0200

    no more time left

---
+++ e/2/A/A_25_Stefan_Grozev/app/views/process_gods/_process_god.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! process_god, :id, :name, :id, :created_at, :updated_at
+json.url process_god_url(process_god, format: :json)
---
+++ e/2/A/A_25_Stefan_Grozev/test/models/process_god_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ProcessGodTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
