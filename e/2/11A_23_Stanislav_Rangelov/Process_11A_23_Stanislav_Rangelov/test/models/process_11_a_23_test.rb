166f7cf198fccc44bedc53f066c61725f4141bf6
StefchoG <ste.grozev.tues19@gmail.com>
2018-03-06 15:40:32 +0200

    Test

---
+++ e/2/11A_23_Stanislav_Rangelov/Process_11A_23_Stanislav_Rangelov/test/models/process_11_a_23_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Process11A23Test < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ e/2/11A_23_Stanislav_Rangelov/Process_11A_23_Stanislav_Rangelov/test/system/process_11_a_23s_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class Process11A23sTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit process_11_a_23s_url
+  #
+  #   assert_selector "h1", text: "Process11A23"
+  # end
+end
