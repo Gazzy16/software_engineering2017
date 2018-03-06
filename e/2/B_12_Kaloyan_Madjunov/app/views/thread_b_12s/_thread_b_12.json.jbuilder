ad52847a66120eabb174fe434e6f4523d5905017
kall0m <k.madjunov@gmail.com>
2018-03-06 16:26:26 +0200

    Add B_12

---
+++ e/2/B_12_Kaloyan_Madjunov/app/views/thread_b_12s/_thread_b_12.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! thread_b_12, :id, :value, :created_at, :updated_at
+json.url thread_b_12_url(thread_b_12, format: :json)
---
+++ e/2/B_12_Kaloyan_Madjunov/test/system/thread_b_12s_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ThreadB12sTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit thread_b_12s_url
+  #
+  #   assert_selector "h1", text: "ThreadB12"
+  # end
+end
