86b7236f98632ef114681d545fbd030cc415c90a
StefchoG <ste.grozev.tues19@gmail.com>
2018-03-07 13:49:46 +0200

    Done

--- e/3/1/B_24_Simeon_Chakarov/db/schema.rb
+++ e/3/1/B_24_Simeon_Chakarov/db/schema.rb
@@ -10,6 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180307113731) do
+
+  create_table "schools", force: :cascade do |t|
+    t.integer "school_id"
+    t.string "school_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "second_name"
+    t.string "email"
+    t.integer "school_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
