b134926a6f5b1d6ac3facd7ed439fb84e22a6b3f
ImTheWizzy <martin_todo9@abv.bg>
2018-03-07 15:36:11 +0200

    commit

--- e/3/3/A_18_Martin_Todorov/config/routes.rb
+++ e/3/3/A_18_Martin_Todorov/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :rooms
+  resources :computers
+  get '11a_18_MartinTodorovsubjects' => 'computers#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
