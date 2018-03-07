114f1548de220ba1e3957a7e97a6e63fef58dabf
karinakozarova <karinakozarova1303@gmail.com>
2018-03-07 15:28:25 +0200

    upd

--- e/3/3/A_12_Karina_Kozarova/Test/app/controllers/schools_controller.rb
+++ e/3/3/A_12_Karina_Kozarova/Test/app/controllers/schools_controller.rb
@@ -1,29 +1,21 @@
 class SchoolsController < ApplicationController
   before_action :set_school, only: [:show, :edit, :update, :destroy]
 
-  # GET /schools
-  # GET /schools.json
   def index
     @schools = School.all
   end
 
-  # GET /schools/1
-  # GET /schools/1.json
   def show
 
   end
 
-  # GET /schools/new
   def new
     @school = School.new
   end
 
-  # GET /schools/1/edit
   def edit
   end
 
-  # POST /schools
-  # POST /schools.json
   def create
     @school = School.new(school_params)
 
@@ -36,8 +28,6 @@ class SchoolsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /schools/1
-  # PATCH/PUT /schools/1.json
   def update
       if @school.update(school_params)
         redirect_to @school, notice: 'School was successfully updated.' 
@@ -46,8 +36,6 @@ class SchoolsController < ApplicationController
       end
   end
 
-  # DELETE /schools/1
-  # DELETE /schools/1.json
   def destroy
     @school.destroy
       redirect_to schools_url, notice: 'School was successfully destroyed.' 
@@ -55,12 +43,10 @@ class SchoolsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_school
       @school = School.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def school_params
       params.require(:school).permit(:school_number)
     end
