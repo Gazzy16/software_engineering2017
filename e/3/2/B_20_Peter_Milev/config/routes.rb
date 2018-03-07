3105c08b8f998923e1ce0f56d3036b16e71ea0c9
Peter Milev <32496132+Pe6oProgramista@users.noreply.github.com>
2018-03-07 14:31:58 +0200

    Update routes.rb
    test

--- e/3/2/B_20_Peter_Milev/config/routes.rb
+++ e/3/2/B_20_Peter_Milev/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :subjects
   resources :teachers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/11b_20_PeterMilevsubjects', to: subjects#index
 end
