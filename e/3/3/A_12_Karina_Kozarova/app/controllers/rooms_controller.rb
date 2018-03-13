58bade2c41f370edb8516ceeb446c1e0b21d82b5
karinakozarova <karinakozarova1303@gmail.com>
2018-03-13 10:31:29 +0200

    fix

--- e/3/3/A_12_Karina_Kozarova/app/controllers/rooms_controller.rb
+++ e/3/3/A_12_Karina_Kozarova/app/controllers/rooms_controller.rb
@@ -6,6 +6,7 @@ class RoomsController < ApplicationController
   # GET /rooms.json
   def index
     @rooms = Room.all
+    @computers = Computer.all
   end
 
   # GET /rooms/1
--- e/3/3/A_12_Karina_Kozarova/config/routes.rb
+++ e/3/3/A_12_Karina_Kozarova/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :rooms
   resources :computers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'A_12_Karina_Kozarova',to: 'rooms#index'
 end
