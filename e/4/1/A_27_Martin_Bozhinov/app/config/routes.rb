2c6be6f124e584f2f033263ced4352dc1cfb464c
martylery <jkata@mail.bg>
2018-03-20 15:21:43 +0200

    after deadline

--- e/4/1/A_27_Martin_Bozhinov/app/config/routes.rb
+++ e/4/1/A_27_Martin_Bozhinov/app/config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'bottle/color:string'
-
-  get 'bottle/brand:string'
-
-  get 'bottle/table_id:integer'
-
+  resources :bottles
   resources :tables
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
