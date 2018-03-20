f255affe34c53f43308fa6200e9b4ae48b4e3015
Kaloyan Madjunov <k.madjunov@gmail.com>
2018-03-20 07:07:15 -0700

    Add some changes so I can presume

--- e/2/B_12_Kaloyan_Madjunov/config/routes.rb
+++ e/2/B_12_Kaloyan_Madjunov/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :program_b_12s
+  resources :program_b_12s, controller: 'program_b_12s'
   resources :thread_b_12s
+
+	root 'program_b_12s#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
