1efa9df497e27e372a64d2058e149efead12c424
Kaloyan Madjunov <k.madjunov@gmail.com>
2018-03-20 08:04:29 -0700

    Submitting

--- e/4/2/Kaloyan_Madjunov_11b_12/app/models/film.rb
+++ e/4/2/Kaloyan_Madjunov_11b_12/app/models/film.rb
@@ -1,2 +1,9 @@
 class Film < ApplicationRecord
+	has_many :cinema_films
+	has_many :cinemas, :through => 'cinema_films'
+
+	#validates :cinema_id, inclusion: { in: Cinema.all.map{|c| c.id},
+    #message: "No such cinema!" }
+
+	validates :name, uniqueness: true
 end
--- e/4/2/Kaloyan_Madjunov_11b_12/config/routes.rb
+++ e/4/2/Kaloyan_Madjunov_11b_12/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :cinema_films
   resources :cinemas
   resources :films
+
+	get "/11b_12_Kaloyan_Madjunovfilms" => "films#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
