ad67ff4b9651c5974ccb8d4e30bede7170ca6141
Loosper <boian4o1@gmail.com>
2018-03-20 15:00:31 +0200

    done

--- e/4/1/A_03_Boian_Karatotev/config/routes.rb
+++ e/4/1/A_03_Boian_Karatotev/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :bottles
   resources :tables
+  get '11a_03_BoianKaratotevbottles', to: 'list_bottles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
