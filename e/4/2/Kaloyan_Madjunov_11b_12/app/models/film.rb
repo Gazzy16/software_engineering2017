1471ce7d90db3e296d262202e37f17a41191df74
Kaloyan Madjunov <k.madjunov@gmail.com>
2018-03-20 17:13:44 +0200

    Update film.rb

--- e/4/2/Kaloyan_Madjunov_11b_12/app/models/film.rb
+++ e/4/2/Kaloyan_Madjunov_11b_12/app/models/film.rb
@@ -2,8 +2,8 @@ class Film < ApplicationRecord
 	has_many :cinema_films
 	has_many :cinemas, :through => 'cinema_films'
 
-	validates :cinema_id, inclusion: { in: Cinema.all.map{|c| c.id},
-    message: "No such cinema!" }
+	#validates :cinema_id, inclusion: { in: Cinema.all.map{|c| c.id},
+    #message: "No such cinema!" }
 
 	validates :name, uniqueness: true
 end
