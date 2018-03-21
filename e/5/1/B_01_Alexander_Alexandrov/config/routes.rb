835806cec6410edef88951b265824a74437b4ca8
kall0m <k.madjunov@gmail.com>
2018-03-21 13:51:48 +0200

    msg

--- e/5/1/B_01_Alexander_Alexandrov/config/routes.rb
+++ e/5/1/B_01_Alexander_Alexandrov/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :client_products
   resources :clients
   resources :products
+  get "/B_01_Alexander_Alexandrov_products/", to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
