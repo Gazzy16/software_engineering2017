dbd0efdc972cb3948540fe68d83ed1725d04be78
viktorgg <vikor.g.g@abv.bg>
2018-04-11 15:15:56 +0300

    commit

--- e/6/2/11A_05_Viktor_Georgiev/App/config/routes.rb
+++ e/6/2/11A_05_Viktor_Georgiev/App/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :schools
   resources :teachers
+  get '/teacher/new', to: 'teachers#new'
+  post '/teacher/new', to: 'teachers#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
