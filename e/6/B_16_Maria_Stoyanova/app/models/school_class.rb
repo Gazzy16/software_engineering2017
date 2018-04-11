7b38d9a281783bd8bb42b6028adff3d613eb6ccb
vladi_marti@cab31 <vladi_marti@cab31>
2018-04-11 13:40:38 +0300

    ne e gotovo 2

--- e/6/B_16_Maria_Stoyanova/app/models/school_class.rb
+++ e/6/B_16_Maria_Stoyanova/app/models/school_class.rb
@@ -1,3 +1,8 @@
 class SchoolClass < ApplicationRecord
 	belongs_to :teacher
+
+	validate :teacher_exists
+
+    	def teacher_exists
+     	errors[:teacher] << "must exist" unless Teacher.exists?(self.teacher_id)
 end
