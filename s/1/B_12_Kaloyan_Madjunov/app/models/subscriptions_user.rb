9a4660dbf23388bf8697d6889741b08ca8965a2f
Kaloyan Madjunov <k.madjunov@gmail.com>
2018-04-16 14:44:31 -0700

    EEEEOOOWWWW

--- s/1/B_12_Kaloyan_Madjunov/app/models/subscriptions_user.rb
+++ s/1/B_12_Kaloyan_Madjunov/app/models/subscriptions_user.rb
@@ -5,30 +5,21 @@ class SubscriptionsUser < ApplicationRecord
 	validates_presence_of :subscription
   validates_presence_of :user
 
-  #validates_numericality_of :subscription, allow_nil: true
-  #validates_numericality_of :user, allow_nil: true
-
-  validate :subscription_price
-
 	validate :validate_roles
 
 private
-
-  def subscription_price
-    if subscription.price < 10
-			
-		end
-  end
-
 	def validate_roles
-    #errors.add(:user, "too much") if user.subscription > 10
-		if self.subscription.price < 10
-			if self.subscription.subscriptions_users.where(role_user :teacher).count > 2
-				errors.add("too much teachers")
+		if subscription.price < 10
+			if self.user_role.downcase == "teacher"
+				if subscription.subscriptions_users.where(user_role: :teacher).count >= 2
+					errors.add(:subscription, "too much teachers")
+				end
 			end
 
-			if self.subscription.subscriptions_users.where(role_user :student).count > 10
-				errors.add("too much students")
+			if self.user_role.downcase == "student"
+				if subscription.subscriptions_users.where(user_role: :student).count >= 10
+					errors.add(:subscription, "too much students")
+				end
 			end
 		end
   end
