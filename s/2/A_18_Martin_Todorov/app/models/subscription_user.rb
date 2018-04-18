fd836b5078cf5da81cf9b4002f0d4912bed525ad
ImTheWizzy <martin_todo9@abv.bg>
2018-04-18 15:19:30 +0300

    working validation

--- s/2/A_18_Martin_Todorov/app/models/subscription_user.rb
+++ s/2/A_18_Martin_Todorov/app/models/subscription_user.rb
@@ -1,5 +1,6 @@
 class SubscriptionUser < ApplicationRecord
     belongs_to :subscription
     belongs_to :user
-    validates :user, uniqueness: true
+    validates :user, uniqueness: { scope: :subscription,
+        message: "Cannot have user with two roles in the same subscription" }
 end
