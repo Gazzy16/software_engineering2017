13173d2e1724bda95a5607ee36f6f1492b2417c0
Your Name <you@example.com>
2018-04-18 14:18:27 +0300

    class

--- s/2/B_19_Peter_Mihaylov/app/models/subscription.rb
+++ s/2/B_19_Peter_Mihaylov/app/models/subscription.rb
@@ -1,4 +1,5 @@
 class Subscription < ApplicationRecord
 	has_many :subscriptions_users
 	has_many :users, :through => 'subscriptions_users'
+	validates_uniqueness_of :user_id, scope: :subscription_id
 end
--- s/2/B_19_Peter_Mihaylov/app/models/subscription_user.rb
+++ s/2/B_19_Peter_Mihaylov/app/models/subscription_user.rb
@@ -1,2 +1,33 @@
 class SubscriptionUser < ApplicationRecord
+	belongs_to :subscription
+	belongs_to :user
+=begin
+	validate :validate_roles
+
+private
+	def validate_roles
+		if subscription.price < 10
+			subscription.price < 10
+			if self.user_role.downcase == "teacher"
+				if subscription.subscription_users.where(user_role: :teacher).count >= 2 && subscription.subcription_users
+					errors.add(:subscription, "too much teachers")
+				end
+
+				if self.user_id == subscription.subscription_users.where(user_role: :student).user_id
+					errors.add(:subscription, "can't subscribe")				
+				end
+			end
+
+			if self.user_role.downcase == "student"
+				if subscription.subscription_users.where(user_role: :student).count >= 10
+					errors.add(:subscription, "too much students")
+				end
+
+				if self.user_id == subscription.subscription_users.where(user_role: :teacher).user_id
+					errors.add(:subscription, "can't subscribe")				
+				end
+			end
+		end	
+	end
+=end
 end
