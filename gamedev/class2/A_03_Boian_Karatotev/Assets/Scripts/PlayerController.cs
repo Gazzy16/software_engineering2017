e70444c3d21dfb2286d12fb40dec61a87837c2ad
Loosper <boian4o1@gmail.com>
2018-05-08 21:28:14 +0300

    I dun it

---
+++ gamedev/class2/A_03_Boian_Karatotev/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public static PlayerController Instance { get; private set; }
+
+    public float speed = 0.2f;
+    public float rotationSpeed = 180f;
+    public Vector3 halfExtents = new Vector3(6, 0, 4);
+
+    private void Start()
+    {
+        AsteroidsManager.Instance.RegisterPlayer(gameObject);
+    }
+
+    void Update () {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Quaternion offset = Quaternion.Euler(0f, horizontalInput * rotationSpeed * Time.deltaTime, 0f);
+        transform.rotation = transform.rotation * offset;
+        Vector3 displacement = new Vector3(0f, 0f, verticalInput) * speed * Time.deltaTime;
+        displacement = transform.rotation * displacement;
+        Vector3 newPosition = transform.position + displacement;
+        for (int i =0; i < 3; ++i)
+        {
+            newPosition[i] = Mathf.Clamp(newPosition[i], -halfExtents[i], halfExtents[i]);
+        }
+        transform.position = newPosition;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Asteroid")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        AsteroidsManager.Instance.UnregisterPlayer(gameObject);
+    }
+}
