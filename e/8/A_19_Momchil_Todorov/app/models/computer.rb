459c71cf533bf32d116dbbc8797f286c84c1cab5
stefan50 <ste.angelov.tues19@gmail.com>
2018-06-20 15:11:13 +0300

    middle

--- e/8/A_19_Momchil_Todorov/app/models/computer.rb
+++ e/8/A_19_Momchil_Todorov/app/models/computer.rb
@@ -1,2 +1,17 @@
 class Computer < ApplicationRecord
+	validate do
+    	if architecture != "x86" && architecture !="x86_64" && architecture != "amd_64"
+      		errors.add(:architecture, "Wrong architecture")
+    	end
+	end
+
+	validate :validates_cars_in_manufacture
+
+	private
+	  	def validates_cars_in_manufacture
+	    	count = Car.where(architecture: architecture, date_manufactured: date_manufactured, manufacture_id: manufacture_id).count
+	    	if count >= 3
+	      		errors.add(:computers, "Max number of computers reached")
+	    	end
+	end
 end
