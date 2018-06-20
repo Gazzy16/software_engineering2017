707527f7e77fb0c608d0ee1a8f8b21f8468dc7f6
stefan50 <ste.angelov.tues19@gmail.com>
2018-06-20 15:17:32 +0300

    final

--- e/8/A_19_Momchil_Todorov/config/routes.rb
+++ e/8/A_19_Momchil_Todorov/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :manufactures
   resources :computers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/A_19_Momchil_Todorov', to: 'computers#index'
 end
