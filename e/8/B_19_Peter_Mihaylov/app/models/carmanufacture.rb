8f92eae8b2cab95e2c5b47833e98b0462998d6dd
pepsm <peps.mihailov@gmail.com>
2018-06-20 13:45:01 +0300

    fds

--- e/8/B_19_Peter_Mihaylov/app/models/carmanufacture.rb
+++ e/8/B_19_Peter_Mihaylov/app/models/carmanufacture.rb
@@ -1,14 +1,14 @@
 class Carmanufacture < ApplicationRecord
 	validate do 
-		if has_car_with_color?	Car.find(self.car_id).color
+		if has_car_reg_color?	Car.find(self.car_id).color
 			errors.add(:team, "there is already a car with this color")
 			end 
 	end 
 	
 		private
-		def has_car_with_color? color
+		def has_car_reg_color? color
 		Manufacture.where(car id: self.car_id).each do |mt|
-			if color==Car.find(mt.car_id).color
+			if color != "red" || color != "green" || color != "blue"
 			return true
 			end
 		end
