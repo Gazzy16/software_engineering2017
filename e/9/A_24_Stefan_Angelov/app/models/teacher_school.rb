e3c8633faf7f756e829a4510c5cb315b7e786a87
stefan50 <ste.angelov.tues19@gmail.com>
2018-06-26 15:23:03 +0300

    Second validation

--- e/9/A_24_Stefan_Angelov/app/models/teacher_school.rb
+++ e/9/A_24_Stefan_Angelov/app/models/teacher_school.rb
@@ -6,6 +6,16 @@ class TeacherSchool < ApplicationRecord
 
   private
     def uniqueness_teacher_subject
-      #count = Teacher.where()
+
+      teacher_schools = TeacherSchool.where(id: school_id)
+      new_teacher = Teacher.where(id: teacher_id)
+
+      for t in teacher_schools
+        t_id = t.teacher_id
+        teacher = Teacher.where(id: t_id)
+        if teacher.subject == new_teacher.subject
+          errors.add(:teacher_id, "has subject")
+        end
+      end
     end
 end
