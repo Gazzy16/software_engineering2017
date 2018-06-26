14c69303a7d321137949b8217322a85f131711fd
simeonch7 <simeonch7@gmail.com>
2018-06-26 16:42:03 +0300

    Done

--- e/9/B_24_Simeon_Chakarov/app/models/teacher_school.rb
+++ e/9/B_24_Simeon_Chakarov/app/models/teacher_school.rb
@@ -7,7 +7,6 @@ class TeacherSchool < ApplicationRecord
     def check
     	current_teacher = TeacherSchool.where(teacher_id: teacher_id)
     	current_school = TeacherSchool.where(school_id: school_id)
-   		current_subject = teacher.subject
    		TeacherSchool.where(teacher_id: current_teacher, school_id: current_school).each do |c|
 			if c.teacher.name.length != c.school.number
 				errors.add(:connect,"Ooops! Error with num->length")
