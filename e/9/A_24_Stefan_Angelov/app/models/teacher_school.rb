70fce9b10ca846f133f9af86705b769b92f0d719
stefan50 <ste.angelov.tues19@gmail.com>
2018-06-26 15:50:37 +0300

    Works now

--- e/9/A_24_Stefan_Angelov/app/models/teacher_school.rb
+++ e/9/A_24_Stefan_Angelov/app/models/teacher_school.rb
@@ -7,12 +7,12 @@ class TeacherSchool < ApplicationRecord
   private
     def uniqueness_teacher_subject
 
-      teacher_schools = TeacherSchool.where(id: school_id)
-      new_teacher = Teacher.where(id: teacher_id)
+      teacher_schools = TeacherSchool.where(school_id: school_id)
+      new_teacher = Teacher.find(teacher_id)
 
       for t in teacher_schools
         t_id = t.teacher_id
-        teacher = Teacher.where(id: t_id)
+        teacher = Teacher.find(t_id)
         if teacher.subject == new_teacher.subject
           errors.add(:teacher_id, "has subject")
         end
