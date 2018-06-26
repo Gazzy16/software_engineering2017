15ca5b92173544d0bc8cd83758e5fe474072d6a4
pepsm <peps.mihailov@gmail.com>
2018-06-26 16:52:07 +0300

    dsf

--- e/9/B_19_Peter_Mihaylov/db/migrate/20180626134514_teachers_schools.rb
+++ e/9/B_19_Peter_Mihaylov/db/migrate/20180626134514_teachers_schools.rb
@@ -1,4 +1,10 @@
 class TeachersSchools < ActiveRecord::Migration[5.2]
   def change
+    create_table :school_teachers do |t|
+      t.integer :school_id
+      t.integer :teacher_id
+
+      t.timestamps
+   end
   end
 end
