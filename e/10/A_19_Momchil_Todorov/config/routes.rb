6fa29f87d54df34cb7128c5663217e20c2264851
momchiltues19 <mom.todorov.tues19@gmail.com>
2018-06-27 15:11:35 +0300

    final

--- e/10/A_19_Momchil_Todorov/config/routes.rb
+++ e/10/A_19_Momchil_Todorov/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :rooms
   resources :hotels
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/A_19_Momchil_Todorov', to: 'rooms#index'
 end
